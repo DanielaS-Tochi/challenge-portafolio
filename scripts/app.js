333b151cc5a0226ac67e448090f2694422a88bc1
DanielaS-Tochi <danielastochi@gmail.com>
2024-10-26 19:59:54 -0300

    Formulario ready in Formspree

--- scripts/app.js
+++ scripts/app.js
@@ -4,16 +4,24 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
     let subject = document.getElementById("subject").value;
     let message = document.getElementById("message").value;
 
+    // Expresión regular para validar el correo electrónico
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
     if (!name || !email || !subject || !message) {
         alert("Todos los campos son obligatorios.");
-        event.preventDefault();
+        event.preventDefault(); // Evita el envío si hay campos vacíos
+    } else if (!email.match(emailPattern)) {
+        alert("Por favor, introduce un correo electrónico válido.");
+        event.preventDefault(); // Evita el envío si el correo no es válido
+    } else {
+        // Formspree manejará el envío si la validación es exitosa
+        alert("Formulario enviado con éxito.");
     }
 });
 
-const burgerMenu = document.querySelector(".burger-menu");
-const navbar = document.querySelector(".navbar");
-
-burgerMenu.addEventListener("click", () => {
+function toggleMenu() {
+    const navbar = document.querySelector(".navbar");
+    const menuLinks = document.querySelector(".menu-links");
     navbar.classList.toggle("open");
-});
-
+    menuLinks.classList.toggle("open");
+}
